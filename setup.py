02a4efc128dd68baab5f485a17d5a2350e4ad337
radin <radin.h@gmail.com>
2024-12-25 22:25:18 -0500

    refactored folders inro src/roksana. updated version number

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ROKSANA',
-    version='0.2.2',
+    version='0.2.3',
     author='Radin Hamidi Rad',
     author_email='radin.h@gmail.com',
     description='A toolkit for keyword search and attack methods on user-provided datasets.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    readme = 'README.md',
     url='https://github.com/radinhamidi/roksana',
     package_dir={'':"src"},
     packages=find_packages("src"),
-    include_package_data=True,
+    # include_package_data=True,
     install_requires=[
         'torch',
         'torch-geometric',
